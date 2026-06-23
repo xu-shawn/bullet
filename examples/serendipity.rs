2ac7cbe1efc23560f7aee8ca7c0a3c05030b31e9
Shawn Xu <xu107288696@gmail.com>
2026-06-22 18:39:41 -0700

    Update config to latest

--- examples/serendipity.rs
+++ examples/serendipity.rs
@@ -1,60 +1,96 @@
 use bullet_lib::{
-    inputs, lr, optimiser, outputs, wdl, Activation, LocalSettings, Loss, TrainerBuilder, TrainingSchedule,
+    game::{
+        inputs::{ChessBuckets, get_num_buckets},
+        outputs::MaterialCount,
+    },
+    nn::{
+        InitSettings, Shape,
+        optimiser::{AdamW, AdamWParams},
+    },
+    trainer::{
+        save::SavedFormat,
+        schedule::{TrainingSchedule, TrainingSteps, lr, wdl},
+        settings::LocalSettings,
+    },
+    value::{ValueTrainerBuilder, loader::DirectSequentialDataLoader},
 };
 
 const HIDDEN_SIZE: usize = 1536;
-const QA: i32 = 255;
-const QB: i32 = 64;
+const SCALE: i32 = 400;
+const QA: i16 = 255;
+const QB: i16 = 64;
+
+const NUM_OUTPUT_BUCKETS: usize = 8;
+#[rustfmt::skip]
+const BUCKET_LAYOUT: [usize; 32] = [
+    0, 0, 1, 1, 2, 2, 3, 3,
+    4, 4, 4, 4, 5, 5, 5, 5,
+    6, 6, 6, 6, 6, 6, 6, 6,
+    6, 6, 6, 6, 6, 6, 6, 6,
+    6, 6, 6, 6, 6, 6, 6, 6,
+    6, 6, 6, 6, 6, 6, 6, 6,
+    6, 6, 6, 6, 6, 6, 6, 6,
+    6, 6, 6, 6, 6, 6, 6, 6,
+];
+const NUM_INPUT_BUCKETS: usize = get_num_buckets(&BUCKET_LAYOUT);
 
 fn main() {
-    let mut trainer = TrainerBuilder::default()
-        .quantisations(&[QA, QB])
-        .optimiser(optimiser::AdamW)
-        .input(inputs::ChessBuckets::new([
-            0, 0, 1, 1, 2, 2, 3, 3,
-            4, 4, 4, 4, 5, 5, 5, 5,
-            6, 6, 6, 6, 6, 6, 6, 6,
-            6, 6, 6, 6, 6, 6, 6, 6,
-            6, 6, 6, 6, 6, 6, 6, 6,
-            6, 6, 6, 6, 6, 6, 6, 6,
-            6, 6, 6, 6, 6, 6, 6, 6,
-            6, 6, 6, 6, 6, 6, 6, 6,
-        ]))
-        .output_buckets(outputs::MaterialCount::<8>::default())
-        .feature_transformer(HIDDEN_SIZE)
-        .activate(Activation::SCReLU)
-        .add_layer(1)
-        .build();
+    let mut trainer = ValueTrainerBuilder::default()
+        .dual_perspective()
+        .optimiser(AdamW)
+        .inputs(ChessBuckets::new(BUCKET_LAYOUT))
+        .output_buckets(MaterialCount::<NUM_OUTPUT_BUCKETS>)
+        .save_format(&[
+            SavedFormat::id("l0w").round().quantise::<i16>(QA),
+            SavedFormat::id("l0b").round().quantise::<i16>(QA),
+            SavedFormat::id("l1w").round().quantise::<i16>(QB),
+            SavedFormat::id("l1b").round().quantise::<i16>(QA * QB),
+        ])
+        .loss_fn(|output, target| output.sigmoid().squared_error(target))
+        .build(|builder, stm_inputs, ntm_inputs| {
+            let l0 = builder.new_affine("l0", 768 * NUM_INPUT_BUCKETS, HIDDEN_SIZE);
+            let l1 = builder.new_affine("l1", 2 * HIDDEN_SIZE, NUM_OUTPUT_BUCKETS);
+
+            let stm_hidden = l0.forward(stm_inputs).screlu();
+            let ntm_hidden = l0.forward(ntm_inputs).screlu();
+            let hidden_layer = stm_hidden.concat(ntm_hidden);
+
+            l1.forward(hidden_layer).select(output_buckets)
+        });
 
     let schedule = TrainingSchedule {
         net_id: "serendipity".to_string(),
-        eval_scale: 400.0,
-        ft_regularisation: 0.0,
-        batch_size: 16_384,
-        batches_per_superbatch: 6104,
-        start_superbatch: 1,
-        end_superbatch: 400,
+        eval_scale: SCALE as f32,
+        steps: TrainingSteps {
+            batch_size: 16_384,
+            batches_per_superbatch: 6104,
+            start_superbatch: 1,
+            end_superbatch: 400,
+        },
         wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
         lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.992, step: 1 },
-        loss_function: Loss::SigmoidMSE,
         save_rate: 1,
-        optimiser_settings: optimiser::AdamWParams {
-            decay: 0.01,
-            beta1: 0.9,
-            beta2: 0.999,
-            min_weight: -1.98,
-            max_weight: 1.98,
-        },
     };
 
-    let settings = LocalSettings {
-        threads: 4,
-        data_file_paths: vec!["/mnt/disks/nnue/nnue_data/test77.bin"],
-        test_set: None,
-        output_directory: "checkpoints",
+    let settings = LocalSettings { threads: 4, test_set: None, output_directory: "checkpoints", batch_queue_size: 64 };
+
+    let data_loader = {
+        use loader::sfbinpack::{MoveType, PieceType, SfBinpackLoader, TrainingDataEntry};
+
+        let file_path = "data/test80-2024-02-feb-2tb7p.min-v2.v6.binpack";
+        let buffer_size_mb = 1024;
+        let threads = 4;
+
+        fn filter(entry: &TrainingDataEntry) -> bool {
+            !entry.pos.is_checked(entry.pos.side_to_move())
+                && entry.score != 32002
+                && entry.pos.piece_at(entry.mv.to()).piece_type() == PieceType::None
+        }
+
+        SfBinpackLoader::new(file_path, buffer_size_mb, threads, filter)
     };
 
-    trainer.run(&schedule, &settings);
+    trainer.run(&schedule, &settings, &data_loader);
 
     for fen in [
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
@@ -65,6 +101,6 @@ fn main() {
     ] {
         let eval = trainer.eval(fen);
         println!("FEN: {fen}");
-        println!("EVAL: {}", 400.0 * eval);
+        println!("EVAL: {}", (SCALE as f32) * eval);
     }
 }
