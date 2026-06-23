328dd9d53e8b8d3fc7fe88dc70c229d55cc4aaf0
Shawn Xu <xu107288696@gmail.com>
2026-06-22 19:08:23 -0700

    path name

--- examples/serendipity.rs
+++ examples/serendipity.rs
@@ -77,7 +77,7 @@ fn main() {
     let data_loader = {
         use loader::sfbinpack::{MoveType, PieceType, SfBinpackLoader, TrainingDataEntry};
 
-        let file_path = "data/test80-2024-02-feb-2tb7p.min-v2.v6.binpack";
+        let file_path = "/workspace/test80-2024-02-feb-2tb7p.min-v2.v6.binpack";
         let buffer_size_mb = 1024;
         let threads = 4;
 
